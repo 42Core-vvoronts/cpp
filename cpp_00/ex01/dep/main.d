901a2b9028444fd1085fe68eba1bc41b2a25d6d7
Victoria Vorontsova <viktoriavorontsova.main@gmail.com>
2025-09-01 20:42:13 +0700

    add: diverge macros for text into separate header

--- cpp_00/ex01/dep/main.d
+++
@@ -1,2 +0,0 @@
-obj/main.o: main.cpp include/Contact.hpp include/main.h \
- include/PhoneBook.hpp include/main.h
--- cpp_00/ex01/include/main.h
+++ cpp_00/ex01/include/main.h
@@ -6,7 +6,7 @@
 /*   By: vvoronts <vvoronts@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 10:29:16 by vvoronts          #+#    #+#             */
-/*   Updated: 2025/09/01 19:47:01 by vvoronts         ###   ########.fr       */
+/*   Updated: 2025/09/01 20:35:57 by vvoronts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,11 @@
 #include <limits>
 #include <vector>
 #include <iomanip>
+#include "PrettyPrint.h"
 
 class Contact;
 
 #define WIDTH 10
-#define ROW "+----------+----------+----------+----------+"
-// Colors and messages
-#define PROMPT  "\033[94m>\033[0m "
-#define ENTER_COMMAND "Enter command: "
-#define HELLO "☎️ \033[94mWelcome to the PhoneBook!\033[0m\n"
-#define BYE "\n☎️ \033[94mBye!\033[0m"
-#define ADD "ADD"
-#define SEARCH "SEARCH"
-#define EXIT "EXIT"
-#define ADDING "Adding a new contact..."
-#define WARNING "\033[91mField cannot be empty!\033[0m"
-#define NAME "Name: "
-#define LASTNAME "Last Name: "
-#define NICKNAME "Nickname: "
-#define PHONE "Phone Number: "
-#define SECRET "Darkest Secret: "
-#define SEARCHING "Enter index: "
-#define SUCCESS "Success!"
-#define HELP "Available commands:\n  ADD — Create a new contact entry\n  SEARCH — Find an existing contact\n  EXIT — Close the application\n"
 
 void align(std::string str, unsigned int width);
 void printEmptyBook(void);
