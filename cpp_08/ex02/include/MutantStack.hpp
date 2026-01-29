295f0829a5a959dc81983dcd6e80eb22f9717cf4
Victoria Vorontsova <viktoriavorontsova.main@gmail.com>
2026-01-29 05:08:11 -0700

    done

--- cpp_08/ex02/include/MutantStack.hpp
+++ cpp_08/ex02/include/MutantStack.hpp
@@ -17,21 +17,42 @@
 
 /**
  * @brief A stack that supports iteration.
+ * Subject:
+ * "MutantStack class. It will be implemented in terms of a std::stack.
+ * It will offer all its member functions, plus an additional feature: iterator"
+ * 
+ * std::stack: https://en.cppreference.com/w/cpp/container/stack.html
+ * 
+ * Deque is the default underlying container for std::stack.
+ * c is the protected member of std::stack that holds the underlying container.
+ * It works the same way as a stack, but also allows iteration through its elements.
+ * 
+ * List of std::stack member functions:
+ * - top: access the top element
+ * - empty: check if the stack is empty
+ * - size: get the number of elements
+ * - push: add an element to the top
+ * - pop: remove the top element
+ *  Added iterators and const versions:
+ * - begin: returns pointer to the First element *it = First element
+ * - end: returns pointer to After last element *it = undefined
+ * - rbegin: returns pointer to the Last element *it = Last element 
+ * - rend: returns pointer to the Before first element *it = undefined
+ * 
  * @tparam T The type of elements stored in the stack.
  * @tparam C The underlying container type (default is std::deque<T>).
- * @details Inherits from std::stack and provides iterator access to the
- * underlying container.
  */
 template <typename T, typename C = std::deque<T> >
-class MutantStack : public std::stack<T, C> 
+class MutantStack : public std::stack<T, C>
 {
     public:
-        // type definition for simpler access to iterator types
-        typedef typename std::stack<T, C>::container_type::iterator iterator;
-        typedef typename std::stack<T, C>::container_type::const_iterator const_iterator;
-        typedef typename std::stack<T, C>::container_type::reverse_iterator reverse_iterator;
+        // type definition for shorter access to iterator types
+        typedef typename std::stack<T, C>::container_type::iterator               iterator;
+        typedef typename std::stack<T, C>::container_type::const_iterator         const_iterator;
+        typedef typename std::stack<T, C>::container_type::reverse_iterator       reverse_iterator;
         typedef typename std::stack<T, C>::container_type::const_reverse_iterator const_reverse_iterator;
 
+        // OCF constructors and destructor, overloaded assignment operator
         MutantStack<T, C>(void) : std::stack<T, C>(){};
         MutantStack<T, C>(MutantStack<T, C> const& src) : std::stack<T, C>(src){};
         ~MutantStack<T, C>(void){};
@@ -41,32 +62,16 @@ class MutantStack : public std::stack<T, C>
             return (*this);
         }
 
-        // iterator functions
-        MutantStack<T, C>::iterator begin(void) { return (this->c.begin()); }
-
-        MutantStack<T, C>::iterator end(void) { return (this->c.end()); }
-
-        // const iterator functions
-        MutantStack<T, C>::const_iterator begin(void) const {
-            return (this->c.begin());
-        }
-
-        MutantStack<T, C>::const_iterator end(void) const { return (this->c.end()); }
-
-        // reverse iterator functions
-        MutantStack<T, C>::reverse_iterator rbegin(void) {
-            return (this->c.rbegin());
-        }
-
-        MutantStack<T, C>::reverse_iterator rend(void) { return (this->c.rend()); }
-
-        // const reverse iterator functions
-        MutantStack<T, C>::const_reverse_iterator rbegin(void) const {
-            return (this->c.rbegin());
-        }
-
-        MutantStack<T, C>::const_reverse_iterator rend(void) const {
-            return (this->c.rend());
-        }
+        // direct 
+        iterator               begin(void) { return (this->c.begin()); }
+        iterator               end(void) { return (this->c.end()); }
+        const_iterator         begin(void) const { return (this->c.begin()); }
+        const_iterator         end(void) const { return (this->c.end()); }
+        // reverse
+        reverse_iterator       rbegin(void) { return (this->c.rbegin()); }
+        reverse_iterator       rend(void) { return (this->c.rend()); }
+        const_reverse_iterator rbegin(void) const { return (this->c.rbegin());}
+        const_reverse_iterator rend(void) const { return (this->c.rend()); }
 };
+
 #endif
--- cpp_08/ex02/src/main.cpp
+++ cpp_08/ex02/src/main.cpp
@@ -16,63 +16,92 @@
 #include <list>
 
 int main() {
-  // SUBJ TEST
-  {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3);
-    mstack.push(5);
-    mstack.push(737);
-    //[...]
-    mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite) {
-      std::cout << *it << std::endl;
+  // SUBJECT  TEST
+    std::cout << "--- Test from subject ---" << std::endl;
+    {
+      MutantStack<int> mstack;
+      mstack.push(5);
+      mstack.push(17);
+      std::cout << mstack.top() << std::endl;
+      mstack.pop();
+      std::cout << mstack.size() << std::endl;
+      mstack.push(3);
+      mstack.push(5);
+      mstack.push(737);
+      //[...]
+      mstack.push(0);
+      MutantStack<int>::iterator it = mstack.begin();
+      MutantStack<int>::iterator ite = mstack.end();
       ++it;
+      --it;
+      while (it != ite) {
+        std::cout << *it << std::endl;
+        ++it;
+      }
+      std::stack<int> s(mstack);
     }
-    std::stack<int> s(mstack);
+    std::cout << std::endl;
+
+  // std::list Comparison (Mandatory check)
+  /* If you run it a first time with your MutantStack, and a second time replacing the
+  MutantStack with, for example, a std::list, the two outputs should be the same. Of
+  course, when testing another container, update the code below with the corresponding
+  member functions (push() can become push_back()).*/
+  std::cout << "\n--- std::list Test (Should match above) ---" << std::endl;
+  {
+    std::list<int> lstack;
+    lstack.push_back(5);
+    lstack.push_back(17);
+    std::cout << lstack.back() << std::endl;
+    lstack.pop_back();
+    std::cout << lstack.size() << std::endl;
+    lstack.push_back(3);
+    lstack.push_back(5);
+    lstack.push_back(737);
+    lstack.push_back(0);
+    std::list<int>::iterator lit = lstack.begin();
+    std::list<int>::iterator lite = lstack.end();
+    ++lit;
+    --lit;
+    while (lit != lite) {
+        std::cout << *lit << std::endl;
+        ++lit;
+    }
+
   }
   std::cout << std::endl;
-  {
-    std::list<int> mstack;
-    mstack.push_back(5);
-    mstack.push_back(17);
-    std::cout << mstack.back() << std::endl;
-    mstack.pop_front();
-    std::cout << mstack.size() << std::endl;
-    mstack.push_back(3);
-    mstack.push_back(5);
-    mstack.push_back(737);
-    //[...]
-    mstack.push_back(0);
-    std::list<int>::iterator it = mstack.begin();
-    std::list<int>::iterator ite = mstack.end();
-    ++it;
-    --it;
-    while (it != ite) {
-      std::cout << *it << std::endl;
-      ++it;
-    }
-     std::stack<int, std::list<int> > s(mstack);
+
+  // ADDITIONAL TESTS
+  std::cout << "\n--- Additional Tests ---" << std::endl;
+    {
+      MutantStack<int> mstack;
+      mstack.push(1);
+      mstack.push(2);
+      mstack.push(3);
+
+      MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+      MutantStack<int>::reverse_iterator rite = mstack.rend();
+      while (rit != rite) {
+          std::cout << *rit << " "; // Should print: 3 2 1
+          ++rit;
+      }
+      std::cout << std::endl;
   }
-  std::cout << std::endl;
+
+  std::cout << "\n--- String Stack Test ---" << std::endl;
   {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(4);
-    mstack.push(3);
-    mstack.push(2);
-    mstack.push(1);
-    for(MutantStack<int>::const_iterator it = mstack.begin(); it < mstack.end(); it++){
-      std::cout << *it << std::endl;
+    MutantStack<std::string> sstack;
+    sstack.push("Hello");
+    sstack.push("World");
+    sstack.push("!");
+
+    MutantStack<std::string>::iterator it = sstack.begin();
+    while (it != sstack.end()) {
+        std::cout << *it << " "; // Output: Hello World ! 
+        it++;
     }
+    std::cout << std::endl;
   }
+
   return 0;
 }
